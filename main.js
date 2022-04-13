55369739531523d2c14248c9a5dccdfe53150e0c
Luis-Alves2 <lema@cin.ufpe.br>
2022-04-12 22:40:19 -0300

    formatting data to 81 entries

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 function getData (url) {
     const data = [];
+    const SenateSet = new Set();
+    const CodParlKey = "ListaParlamentarEmExercicio.Parlamentares.Parlamentar.IdentificacaoParlamentar.CodigoParlamentar";
+
+    var pastRow;
+    var i = 0;
+
     d3.dsv(';', url, (row) => {
-        data.push(row);
+
+        if (i != 0) {
+            if (row[CodParlKey] != pastRow[CodParlKey]) {
+                if (pastRow[CodParlKey] != "") {
+                    data.push(pastRow);
+                }
+            }
+        }
+
+        pastRow = row
+        i = 1;
     });
+
+
+    //console.log(data.sort())
     return data;
+}
+
+function getParties() {
+    return
 }
 
 function main () {  
     const dataTable = getData(dataUrl);
-    console.log(dataTable);
+    //console.log(dataTable);
 }
 
 const dataUrl = 'https://raw.githubusercontent.com/nivan/testPython/main/ListaParlamentarEmExercicio.csv';
